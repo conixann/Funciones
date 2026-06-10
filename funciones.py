dd31cf344bc140eb0d55d5fa96455363cc2b1090
conixann <mil.elgueta@duocuc.cl>
2026-06-10 11:05:21 -0400

    inicio funciones

---
+++ funciones.py
@@ -0,0 +1,15 @@
+#1. Función sin parámetros:
+def saludar():
+    print("¡Bienvenido al sistema!")
+
+saludar()
+saludar()
+saludar()
+
+
+#2. Función con parámetros:
+def saludar_usuario(nombre):
+    print(f"¡Hola, {nombre}! Bienvenido.")
+
+saludar_usuario("Ana")
+saludar_usuario("Luis")
