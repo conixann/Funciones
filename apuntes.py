ada7bc208063e443535b82722de38504e2a04eee
conixann <mil.elgueta@duocuc.cl>
2026-06-10 11:29:10 -0400

    funciones a la mitad

---
+++ apuntes.py
@@ -0,0 +1,22 @@
+"""Una función es un bloque de código """
+#para que sirve una  función? 
+#para reutilizar codigo
+
+#1
+#def nombre función (parametros/argumentos): Crea o declara una función
+#Invoca usando un nombre
+#se usa el nombre de función() si no sera tomado como una variable
+#No necesita parametros para trabajar
+
+#2
+#Función con prametros (nunca tener 2 funciones con el mismo nombre si no tira error)
+#Esta función necesita que le entregues un valor, necesita un parametro para trabajar
+#debo entregar valor si tiene parametros, de donde viene el valor? de un input, de una lista o de un diccionario, etc
+#el parametro solo funciona dentro de la función
+
+#3
+#el comando return me devuelve un valor, no se veria nada si lo imprimo a secas 
+#las variables creadas dentro de una función no sirven afuera de la función, se puede validar una funcion con un try except
+
+#4 
+#la diferencia es que el 2do parametro tiene un valor, no le puedo dar un valor por defecto a el primer parametro, siempre a los ultimos que existan
--- funciones.py
+++ funciones.py
@@ -13,3 +13,20 @@ def saludar_usuario(nombre):
 
 saludar_usuario("Ana")
 saludar_usuario("Luis")
+
+#3. Función con retorno:
+def sumar(a,b):
+    resultado= a + b
+    return resultado
+total = sumar(5,3)
+print(f"La suma es: {total}")
+print(sumar(10,20))
+print(sumar("hola","mundo"))
+
+#4. Parámetros con valor por defecto:
+def crear_usuario(nombre, rol="estudiante"):
+    print(f"Usuario: {nombre} | rol: {rol}")
+
+crear_usuario("Maria")
+crear_usuario("Pedro","Administrador")
+crear_usuario("Laura", rol="Docente")
